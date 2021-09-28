cd91a39ac0e798ead0c5945ed95d99e27dbcbabb
马涛 <449636697@qq.com>
2021-09-28 11:14:11 +0800

    新建测试工程

---
+++ 2021TestProjTests/_021TestProjTests.swift
@@ -0,0 +1,33 @@
+//
+//  _021TestProjTests.swift
+//  2021TestProjTests
+//
+//  Created by 艾泽鑫 on 2021/9/28.
+//
+
+import XCTest
+@testable import _021TestProj
+
+class _021TestProjTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
